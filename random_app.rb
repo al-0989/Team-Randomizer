f21bf164269ad308011ce91b0775843e3b28edb3
Jennifer Li <ldnjennifer@hotmail.com>
2016-01-29 10:19:22 -0800

    this commit will modify random.css, random_app.rb, pick_rand.erb

--- random_app.rb
+++ random_app.rb
@@ -1,38 +1,56 @@
 require "sinatra"
 require "sinatra/reloader"
 
+
+enable :sessions
+
 get "/" do
+  @names = session[:names]
+  @num_of_team = session[:num_of_team]
   erb :pick_rand, layout: :random_layout
 end
 
+get "/pick_random" do
+  redirect to "/"
+end
+
 post "/pick_random" do
-  @arr_of_teams = []
-  @students_arr = params[:names].split(",")
+
+  @names = params[:names]
+  session[:names] = @names
+  session[:arr_of_teams] = []
+
+  students_arr = params[:names].split(",")
+
   @num_of_team = params[:team_count].to_i
-  shuffled_names_arr = shuffleNames(@students_arr)
+  session[:num_of_team]=@num_of_team
+
+  shuffled_names_arr = shuffleNames(students_arr)
+
   method = params[:method]
 
-  if @num_of_team > @students_arr.length
-    p @arr_of_teams << "Invalid! Number of teams is greater than the number of students entered."
+  if @num_of_team > students_arr.length
+    session[:arr_of_teams]<< "Invalid! Number of teams is greater than the number of students entered."
   end
 
   if method == "count"
     @num_of_team.times do
-       @arr_of_teams << [shuffled_names_arr.pop]
+      session[:arr_of_teams] << [shuffled_names_arr.pop]
     end
 
     while !shuffled_names_arr.empty?
-        @arr_of_teams.each do |x|
-          next if shuffled_names_arr.empty?
-          p x << shuffled_names_arr.pop
-        end
-        break if shuffled_names_arr.empty?
+      session[:arr_of_teams].each do |x|
+        next if shuffled_names_arr.empty?
+        p x << shuffled_names_arr.pop
+      end
+      break if shuffled_names_arr.empty?
     end
   else
-    @arr_of_teams = shuffled_names_arr.each_slice(@num_of_team).to_a
+    session[:arr_of_teams] = shuffled_names_arr.each_slice(@num_of_team).to_a
   end
 
-  erb :pick_rand, layout: :random_layout
+  redirect to "/"
+  # erb :pick_rand, layout: :random_layout
 end
 
 ## Method used to shuffle names
@@ -44,5 +62,9 @@ def shuffleNames(arr)
     arr[i] = arr[random_student];
     arr[random_student] = tempHold;
   end
-  arr.compact
+  if  arr.compact! == nil
+    []
+  else
+    arr
+  end
 end
